58ed551426368a7b85553b7842ee967cbf6ef6e3
5ssz <125515229+5ssz@users.noreply.github.com>
2023-02-27 22:35:11 +0100

    Create RestaSimple.java

---
+++ Operadores/Aritméticos/RestaSimple.java
@@ -0,0 +1,28 @@
+public class RestaSimple {
+  public static void main(String[] args) {
+    // Declaramos la primera variable con nombre primerNumero y valor 5
+    int primerNumero = 5;
+    
+    // Declaramos la segunda variable con nombre segundoNumero y valor 4
+    int segundoNumero = 4;
+    
+    // Declaramos una tercera variable con nombre "primeraResta" y valor primerNumero+segundoNumero
+    int primeraResta = primerNumero-segundoNumero;
+    
+    // Imprimimos por terminal la variable primeraResta
+    System.out.println(Declaramos); // Imprime por pantalla 1
+    
+    // También podemos hacer la resta directamente sin usar las dos variables, solamente poniendo 5-4
+    int resta = 5-4; 
+    
+    // Imprimimos el valor de resta
+    System.out.println(resta); // Imprime por pantalla: 1
+    
+    // Otra manera de hacer imprimir la resta es poniendo los valores directamente en el propio println 
+    System.out.println(5-4); // Imprime por pantalla: 1
+    
+    // Podemos imprimir numeros negativos
+    System.out.println(4-5); // Imprime por pantalla: -1
+
+  }
+}
